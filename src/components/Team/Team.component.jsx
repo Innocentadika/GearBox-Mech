9390e344990b29b5473e5663a38618eb8564e221
innocentadika <innocentadika18@gmail.com>
2023-02-22 23:51:18 +0300

    langing desktop responsive 85% complete

--- src/components/Team/Team.component.jsx
+++ src/components/Team/Team.component.jsx
@@ -3,31 +3,31 @@ import WireFlower from '../assets/7.png'
 const Team = () => {
   return (
     <div>
-      <div className='flex-col p-2 space-y-2 bg-zinc-900'>
-        <div className='flex space-x-6 rounded  bg-slate-200'>
-        <div className=''>
+      <div className='flex-col p-2  space-y-2 bg-zinc-900 xl:text-lg'>
+        <div className='flex space-x-6 rounded  bg-slate-200 xl:h-64 '>
+        <div className=' '>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu sed suspendisse in pulvinar. Amet, amet, eleifend lacinia consectetur. In.</p>
         </div>
-        <div>
-        <img className='rounded' src={WireFlower} alt="description1" />
+        <div className=''>
+        <img className='rounded xl:h-64 xl:w-96' src={WireFlower} alt="description1" />
         </div>
         </div>
 
-        <div className='flex space-x-6 rounded  bg-slate-200'>
+        <div className='flex space-x-6 rounded  bg-slate-200 xl:h-64 '>
         <div>
-        <img className='rounded' src={WireFlower} alt="description1" />
+        <img className='rounded xl:h-64 xl:w-96' src={WireFlower} alt="description1" />
         </div>
         <div className=''>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu sed suspendisse in pulvinar. Amet, amet, eleifend lacinia consectetur. In.</p>
         </div>
         </div>
 
-        <div className='flex space-x-6 rounded  bg-slate-200'>
-        <div className=''>
+        <div className='flex space-x-6 rounded  bg-slate-200 xl:h-64 '>
+        <div className=' '>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu sed suspendisse in pulvinar. Amet, amet, eleifend lacinia consectetur. In.</p>
         </div>
-        <div>
-        <img className='rounded' src={WireFlower} alt="description1" />
+        <div className=''>
+        <img className='rounded xl:h-64 xl:w-96' src={WireFlower} alt="description1" />
         </div>
         </div>
       </div>
