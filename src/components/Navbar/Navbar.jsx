6e03efea650032b57fb8b1fbb18a362b0085d331
innocentadika <innocentadika18@gmail.com>
2023-03-03 10:39:06 +0300

    addittion of padding to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <div>
         <div className='flex space-x-1 font-serif bg-zinc-600'>
-<Link className='bg-slate-400 rounded' to='/'>Home</Link>
-<Link className='bg-slate-400 rounded' to='/contacts'>Contact Us</Link>
-<Link className='bg-slate-400 rounded' to='/more'>More</Link>
+<Link className='bg-slate-400 rounded px-1' to='/'>Home</Link>
+<Link className='bg-slate-400 rounded px-1' to='/contacts'>Contact Us</Link>
+<Link className='bg-slate-400 rounded px-1' to='/more'>Gallery</Link>
         </div>
       <Outlet />
     </div>
